557714a48a296e041a0c4fda4e79efe78fd8875e
Carlos <carloseduardortega@gmail.com>
2020-07-26 18:39:39 -0400

    prin de clase search

--- src/view/Search.java
+++ src/view/Search.java
@@ -1,14 +1,14 @@
 package view;
 
 import javax.swing.JPanel;
+import java.awt.Color;
 
 public class Search extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
+	
 	public Search() {
-
+		setBackground(new Color(176, 224, 230));
+		
 	}
 
 }
